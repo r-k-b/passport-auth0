4ec24c7b527f7b29367b3e58e682c596a3c4b99a
Robert K. Bell <r-k-b@users.noreply.github.com>
2018-01-18 08:49:40 +1100

    Add initial TypeScript definition file

---
+++ index.d.ts
@@ -0,0 +1,65 @@
+// Type definitions for passport-auth0, based on @types/passport-facebook
+
+import passport = require('passport')
+import express = require('express')
+
+export interface Profile extends passport.Profile {
+  id: string
+  displayName: string
+  gender?: string
+  ageRange?: {
+    min: number
+    max?: number
+  }
+  profileUrl?: string
+  username?: string
+  birthday: string
+
+  _raw: string
+  _json: any
+}
+
+export interface AuthenticateOptions extends passport.AuthenticateOptions {
+  authType?: string
+}
+
+export interface StrategyOption {
+  domain: string
+  clientID: string
+  clientSecret: string
+  callbackURL: string
+
+  scopeSeparator?: string
+  enableProof?: boolean
+  profileFields?: string[]
+}
+
+export interface StrategyOptionWithRequest extends StrategyOption {
+  passReqToCallback: true
+}
+
+export type VerifyFunction = (
+  accessToken: string,
+  refreshToken: string,
+  profile: Profile,
+  done: (error: any, user?: any, info?: any) => void,
+) => void
+
+export type VerifyFunctionWithRequest = (
+  req: express.Request,
+  accessToken: string,
+  refreshToken: string,
+  profile: Profile,
+  done: (error: any, user?: any, info?: any) => void,
+) => void
+
+export class Strategy implements passport.Strategy {
+  constructor(
+    options: StrategyOptionWithRequest,
+    verify: VerifyFunctionWithRequest,
+  )
+  constructor(options: StrategyOption, verify: VerifyFunction)
+
+  name: string
+  authenticate: (req: express.Request, options?: object) => void
+}
